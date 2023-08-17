b1e6a805265b22e82b947ce5b93fec043030db74
k-suke39 <pz.mozarla@gmail.com>
2023-08-17 02:04:08 +0000

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
   resources :users
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
 end
